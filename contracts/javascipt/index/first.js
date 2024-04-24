8e8775b01649e15b5c9824aa5dae1152c218d6e4
hemant <hemantkumar@gmail.com>
2024-04-24 14:54:23 +0530

    promise

---
+++ contracts/javascipt/index/first.js
@@ -0,0 +1,15 @@
+function asyncfunc(){
+    //     return new Promise((resolve,reject)=>{
+    //         setTimeout(()=>{
+    //             console.log("some data1");
+    //             resolve("success")
+    
+    //         },2000)
+    //     })
+    // }
+    // console.log("fetching some data 1")
+    
+    // let p1 = asyncfunc();
+    // p1.then((res)=>{
+    //     console.log(res);
+    // })
